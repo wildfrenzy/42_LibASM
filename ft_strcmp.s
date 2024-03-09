9893325168418911f96f388058947e7258590804
Esperanza <wildfrenzy0432@gmail.com>
2024-03-09 01:57:06 +0100

    fixed strcmp

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -1,20 +1,26 @@
 ; rdi - first arg
 ; rsi - second arg
 ; rax - return
-; r10 - tmp
+; r8 - tmp
 
 		global		ft_strcmp
 		section		.text
 
 ft_strcmp:
-		cmp			rdi, rsi
+		mov			rax, 0
+		mov			r8, 0
+
+loop:
+		mov			al, byte[rdi] ; lowest 8-bits of rax
+		mov			r8b, byte[rsi]
+		cmp			al, r8b
 		jne			return
+		cmp			al, 0
+		je			return
 		inc			rdi
 		inc			rsi
-		je 			ft_strcmp
+		jmp			loop
 
 return:
-		mov			r10, [rdi]
-		sub			r10, [rsi]
-		mov			rax, r10
+		sub			rax, r8
 		ret
